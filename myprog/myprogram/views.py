59945ec023f6a6b92dfc615c88fabe26418b23d7
Egor Levashov <112577182+sxfour@users.noreply.github.com>
2023-12-17 22:50:53 +0300

    Add files via upload

---
+++ myprog/myprogram/views.py
@@ -0,0 +1,56 @@
+from django.http import HttpResponse, HttpResponseNotFound
+from django.template.defaultfilters import slugify
+from django.template.loader import render_to_string
+from django.shortcuts import render, redirect
+from django.urls import reverse
+
+menu = [
+    {'title': 'О сайте', 'url_name': 'about'},
+    {'title': 'Добавить статью', 'url_name': 'add_page'},
+    {'title': 'Обратная связь', 'url_name': 'contact'},
+    {'title': 'Войти', 'url_name': 'login'},
+        ]
+
+data_db = [
+    {'id': 1, 'title': 'Анджелина Джоли', 'content': '''<h1>Анджелина Джоли</h1> (англ. Angelina Jolie[7], при рождении Войт (англ. Voight), ранее Джоли Питт (англ. Jolie Pitt); род. 4 июня 1975, Лос-Анджелес, Калифорния, США) — американская актриса кино, телевидения и озвучивания, кинорежиссёр, сценаристка, продюсер, фотомодель, посол доброй воли ООН.
+        Обладательница премии «Оскар», трёх премий «Золотой глобус» (первая актриса в истории, три года подряд выигравшая премию) и двух «Премий Гильдии киноактёров США».''',
+     'is_published': True},
+    {'id': 2, 'title': 'Марго Робби', 'content': 'Биография Марго Робби', 'is_published': False},
+    {'id': 3, 'title': 'Джулия Робертс', 'content': 'Биография Джулии Робертс', 'is_published': True},
+]
+
+
+# http request
+def index(request):
+    data = {
+        'title': 'Главная страница',
+        'menu': menu,
+        'posts': data_db,
+    }
+    return render(request, 'program/index.html', context=data)
+
+
+def about(request):
+    return render(request, 'program/about.html', {'title': 'О Сайте', 'menu': menu})
+
+
+def show_post(request, post_id):
+    return HttpResponse(f'id = {post_id}')
+
+
+def addpage(request):
+    return HttpResponse('Добавить статью')
+
+
+def contact(request):
+    return HttpResponse('Обратная связь')
+
+
+def login(request):
+    return HttpResponse('Войти')
+
+
+def page_not_found(request, exception):
+    return HttpResponseNotFound('<h1 align="center" style="color:rgb(169,169,169)"> '
+                                '— Ну вот, я так и думал.'
+                                ' С этой стороны ничуть не лучше...</h1>')
