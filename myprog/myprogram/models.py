051dc1bc208ab7f14b5be0ceaf87813eff22e409
Egor Levashov <112577182+sxfour@users.noreply.github.com>
2023-12-19 19:02:41 +0300

    Delete myprog directory

--- myprog/myprogram/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class Program(models.Model):
-    title = models.CharField(max_length=255)
-    content = models.TextField(blank=True)
-    time_create = models.DateTimeField(auto_now_add=True)
-    time_update = models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField(default=True)
