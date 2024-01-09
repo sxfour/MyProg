1cdb13c8af3ffe63cd0564b2c145bcd53bff07c5
Egor Levashov <112577182+sxfour@users.noreply.github.com>
2024-01-09 08:06:59 +0300

    Delete myprog directory

--- myprog/myprogram/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class MyprogramConfig(AppConfig):
-    verbose_name = 'Моя программа'
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'myprogram'
