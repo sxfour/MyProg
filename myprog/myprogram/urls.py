abea14ff95f0871754d7d80627a3dfe4028d6251
Egor Levashov <112577182+sxfour@users.noreply.github.com>
2024-01-13 16:42:29 +0300

    Add files via upload

---
+++ myprog/myprogram/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path, register_converter
+from myprogram import views, converters
+
+register_converter(converters.FourDigitYearConverter,'year4')
+
+urlpatterns = [
+    path('', views.ProgramHome.as_view(), name='home'),
+    path('about/', views.about, name='about'),
+    path('addpage/', views.AddPage.as_view(), name='add_page'),
+    path('contact/', views.contact, name='contact'),
+    path('login/', views.login, name='login'),
+    path('post/<slug:post_slug>/', views.ShowPost.as_view(), name='post'),
+    path('category/<slug:cat_slug>', views.ProgramCategory.as_view(), name='category'),
+    path('tag/<slug:tag_slug>/', views.TagPostList.as_view(), name='tag'),
+]
